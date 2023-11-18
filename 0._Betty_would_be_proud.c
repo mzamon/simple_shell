65f4755544d5fbcc7a814a734921837e452815c0
mzamon <ndlovu.richmond@gmail.com>
2023-11-18 13:52:15 -0800

    update

---
+++ 0._Betty_would_be_proud.c
@@ -0,0 +1,15 @@
+#include 'simpleshell.h'
+
+/**
+*main is - The Entry Point
+*Objective: program that prints "Hello, Betty!"
+*When Return:0 then = {success}
+*
+*/
+
+int main(void)
+
+{
+        printf('Hello, Betty!\n');
+        return (0);
+}
